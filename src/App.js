c8968c5bc0a4246b625c3565d0a48c213954ec12
tiannapalumbo <74277795+tiannapalumbo@users.noreply.github.com>
2021-02-06 14:44:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Hello world
+        </h1>
+        <Weather city="Paris"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} degrees C`);
+    }
+    let apiKey = "6e8ce867bc46f41d0e8f2b0e41afed08";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (
+      <h2> Hello from the weather</h2>
+    )
+}
