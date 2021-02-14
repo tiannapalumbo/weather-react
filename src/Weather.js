b04cb6caa67755572ec74e770f83925c4bb4c8a6
tiannapalumbo <74277795+tiannapalumbo@users.noreply.github.com>
2021-02-14 14:23:17 -0500

    added enhanced search and location buttons, styled w bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -5,25 +5,32 @@ import axios from "axios";
 export default function Weather() {
 
     return (
-      <div className="Weather"> 
-      <form> 
-        <div className="row">
-            <div className="col-9">
-          <input 
-          type="search" 
-          placeholder="Search for a City..." 
-          className="form-control"
-          />
+        <div className="Weather">
+        <form>
+          <div className="form-row align-items-center">
+            <div className="col-10">
+              <input
+                type="search"
+                className="form-control form-control-sm shadow-sm border-0"
+                placeholder="Search for a city"
+                autoFocus="on"
+                autoComplete="off"
+              />
             </div>
-            <div className="col-3">
-          <input 
-          type="submit" 
-          value="Search" 
-          className="btn btn-light" 
-          />
+            <div className="col-1">
+              <button
+                type="submit"
+                className="form-control-sm btn btn-sm btn-light shadow-sm border-0 searchIcon">
+                <i className="fas fa-search search-icon" />
+              </button>
             </div>
-        </div>
-      </form>
+            <div className="col-1">
+              <button type="button" className="btn btn-sm btn-outline-light shadow-sm" >
+                <i className="fas fa-map-marker-alt" />
+              </button>
+            </div>
+          </div>
+        </form>
       <h1> New York </h1>
       <ul> 
           <li> Monday 07:00 </li>
